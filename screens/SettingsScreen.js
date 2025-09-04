565581479739d1633c504f6c793b45e237723713
f1shuu <szulzyckifilip@gmail.com>
2025-09-04 10:40:30 +0200

    Add the option to restore default settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -15,22 +15,31 @@ import Colors from '../constants/Colors';
 var pkg = require('../package.json');
 
 export default function SettingsScreen() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [isConfirmationModalVisible, setIsConfirmationModalVisible] = useState(false);
+    const [isModal1Visible, setIsModal1Visible] = useState(false);
+    const [isModal2Visible, setIsModal2Visible] = useState(false);
+    const [isConfirmationModal1Visible, setIsConfirmationModal1Visible] = useState(false);
+    const [isConfirmationModal2Visible, setIsConfirmationModal2Visible] = useState(false);
 
-    const { getColor, translate } = useSettings();
+    const { getColor, restoreDefault, translate } = useSettings();
 
     const navigation = useNavigation();
 
-    const handleModal = () => {
+    const handleModal = (modalID) => {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Error);
-        setIsModalVisible(!isModalVisible);
+        if (modalID === 1) setIsModal1Visible(!isModal1Visible);
+        else setIsModal2Visible(!isModal2Visible);
     }
 
     const deleteAllPasswords = async () => {
         await SecureStore.deleteItemAsync('passwords');
-        setIsModalVisible(false);
-        setIsConfirmationModalVisible(true);
+        setIsModal1Visible(false);
+        setIsConfirmationModal1Visible(true);
+    }
+
+    const restoreDefaultSettings = () => {
+        restoreDefault();
+        setIsModal2Visible(false);
+        setIsConfirmationModal2Visible(true);
     }
 
     const styles = {
@@ -48,23 +57,40 @@ export default function SettingsScreen() {
         <Container>
             <Setting name={translate('language')} icon={'globe'} color={getColor('text')} type='navigate' onPress={() => navigation.navigate('SelectionScreen', { screen: 'language' })} />
             <Setting name={translate('theme')} icon={'palette'} color={getColor('text')} type='navigate' onPress={() => navigation.navigate('SelectionScreen', { screen: 'theme' })} />
-            <Setting name={translate('deleteAllPasswords')} icon={'trash-can'} color={Colors.red} onPress={handleModal} />
+            <Setting name={translate('deleteAllPasswords')} icon={'trash-can'} color={Colors.red} onPress={() => handleModal(1)} />
+            <Setting name={translate('restoreDefault')} icon={'rotate-right'} color={getColor('text')} onPress={() => handleModal(2)} />
             <Text style={styles.text}>v{pkg.version}</Text>
             <Modal
-                isVisible={isModalVisible}
+                isVisible={isModal1Visible}
                 text={translate('areYouSureYouWantToDeleteAllPasswords')}
                 twoButtons={true}
                 buttonOneText={translate('yes')}
                 buttonOneOnPress={deleteAllPasswords}
                 buttonTwoText={translate('no')}
-                buttonTwoOnPress={() => setIsModalVisible(!isModalVisible)}
+                buttonTwoOnPress={() => setIsModal1Visible(!isModal1Visible)}
             />
             <Modal
-                isVisible={isConfirmationModalVisible}
+                isVisible={isConfirmationModal1Visible}
                 text={translate('allPasswordsDeletedSuccessfully')}
                 twoButtons={false}
                 buttonOneText={translate('ok')}
-                buttonOneOnPress={() => setIsConfirmationModalVisible(!isConfirmationModalVisible)}
+                buttonOneOnPress={() => setIsConfirmationModal1Visible(!isConfirmationModal1Visible)}
+            />
+            <Modal
+                isVisible={isModal2Visible}
+                text={translate('areYouSureYouWantToRestoreDefaultSettings')}
+                twoButtons={true}
+                buttonOneText={translate('yes')}
+                buttonOneOnPress={restoreDefaultSettings}
+                buttonTwoText={translate('no')}
+                buttonTwoOnPress={() => setIsModal2Visible(!isModal2Visible)}
+            />
+            <Modal
+                isVisible={isConfirmationModal2Visible}
+                text={translate('defaultSettingsSuccessfullyRestored')}
+                twoButtons={false}
+                buttonOneText={translate('ok')}
+                buttonOneOnPress={() => setIsConfirmationModal2Visible(!isConfirmationModal2Visible)}
             />
         </Container>
     )
